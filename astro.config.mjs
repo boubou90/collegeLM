b201661a0731e85e156a92ee859cd9c0e4dd2f3b
boubou90 <34448542+boubou90@users.noreply.github.com>
2025-01-21 12:40:31 +0100

    Update astro.config.mjs
    ref8

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://votre-site.netlify.app',
+  site: 'https://collegelouisemichel.netlify.app',
   integrations: [
     react(), 
     tailwind(),
